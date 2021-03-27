1cd66b457c5522b6fdc9842eb137bed9113ae04b
Umut <umutsahin@protonmail.com>
2021-03-27 11:49:19 +0300

    split status and result into separate modules

--- include/encin/status.h
+++ include/encin/status.h
@@ -1,15 +1,7 @@
 #pragma once
 
-typedef enum {
-
-    ENCIN_SUCCESS = 0,
-
-    ENCIN_STACK_ACQUIRY_FAILURE = -1,
-
-} encin_result;
+#define encin_status (*encin_status_pointer())
 
 encin_result *encin_status_pointer();
 
 const char *encin_status_description();
-
-#define encin_status (*encin_status_pointer())
