fad1b3bd3df4099560e10659f03783a86fad71dc
Umut <umutsahin@protonmail.com>
2021-03-27 11:50:12 +0300

    fix static assertion in await macro

--- include/encin/await.h
+++ include/encin/await.h
@@ -4,7 +4,4 @@
 #include <stdbool.h>
 
 #define encin_await(task)                                                                          \
-    static_assert(false, "encin_await is not implemented yet");
-
-#define encin_awaitv(task)                                                                         \
-    static_assert(false, "encin_awaitv is not implemented yet");
+    ({ static_assert(false, "encin_await is not implemented yet"); })
