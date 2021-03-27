e330203f11a21d3668266c229a0e4aafcfd7599d
Umut <umutsahin@protonmail.com>
2021-03-27 11:51:46 +0300

    abuse the C preprocessor to improve ergonomics of spawning tasks (closes #1)

--- include/encin/spawn.h
+++ include/encin/spawn.h
@@ -3,138 +3,166 @@
 #include <assert.h>
 #include <stdbool.h>
 
-#define encin_spawner()                                                                            \
-    static_assert(false, "encin_spawner is not implemented yet");
+#define encin_spawn(...)                                                                           \
+    encin_concat(encin_spawn_, encin_count_arguments(__VA_ARGS__))(__VA_ARGS__)
+
+#define encin_spawn_blocking(...)                                                                  \
+    encin_concat(encin_spawn_blocking_, encin_count_arguments(__VA_ARGS__))(__VA_ARGS__)
+
+#define encin_spawn_void(...)                                                                      \
+    encin_concat(encin_spawn_void_, encin_count_arguments(__VA_ARGS__))(__VA_ARGS__)
+
+#define encin_spawn_blocking_void(...)                                                             \
+    encin_concat(encin_spawn_blocking_void_, encin_count_arguments(__VA_ARGS__))(__VA_ARGS__)
+
+#define encin_spawn_detached(...)                                                                  \
+    encin_concat(encin_spawn_detached_, encin_count_arguments(__VA_ARGS__))(__VA_ARGS__)
+
+#define encin_spawn_blocking_detached(...)                                                         \
+    encin_concat(encin_spawn_blocking_detached_, encin_count_arguments(__VA_ARGS__))(__VA_ARGS__)
 
 
-#define encin_spawn0(function, stack_size)                                                         \
-    static_assert(false, "encin_spawn0 is not implemented yet");
+#define encin_concat(x, y)                                                                         \
+    encin_concat_hack(x, y)
 
-#define encin_spawn0v(function, stack_size)                                                        \
-    static_assert(false, "encin_spawn0v is not implemented yet");
+#define encin_concat_hack(x, y)                                                                    \
+    x ## y
 
-#define encin_spawn0d(function, stack_size)                                                        \
-    static_assert(false, "encin_spawn0d is not implemented yet");
+#define encin_count_arguments(...)                                                                 \
+    encin_count_arguments_hack(__VA_ARGS__, 6, 5, 4, 3, 2, 1, 0, is_used_incorrectly)
 
-#define encin_spawn0_blocking(function, stack_size)                                                \
-    static_assert(false, "encin_spawn0_blocking is not implemented yet");
-
-#define encin_spawn0v_blocking(function, stack_size)                                               \
-    static_assert(false, "encin_spawn0v_blocking is not implemented yet");
-
-#define encin_spawn0d_blocking(function, stack_size)                                               \
-    static_assert(false, "encin_spawn0d_blocking is not implemented yet");
+#define encin_count_arguments_hack(_function, _1, _2, _3, _4, _5, _6, _stack_size, n, ...)         \
+    n
 
 
-#define encin_spawn1(function, arg1, stack_size)                                                   \
-    static_assert(false, "encin_spawn1 is not implemented yet");
+#define encin_spawn_0(function, stack_size)                                                        \
+    ({ static_assert(false, "encin_spawn is not implemented yet"); })
 
-#define encin_spawn1v(function, arg1, stack_size)                                                  \
-    static_assert(false, "encin_spawn1v is not implemented yet");
+#define encin_spawn_blocking_0(function, stack_size)                                               \
+    ({ static_assert(false, "encin_spawn_blocking is not implemented yet"); })
 
-#define encin_spawn1d(function, arg1, stack_size)                                                  \
-    static_assert(false, "encin_spawn1d is not implemented yet");
+#define encin_spawn_void_0(function, stack_size)                                                   \
+    ({ static_assert(false, "encin_spawn_void is not implemented yet"); })
 
-#define encin_spawn1_blocking(function, arg1, stack_size)                                          \
-    static_assert(false, "encin_spawn1_blocking is not implemented yet");
+#define encin_spawn_blocking_void_0(function, stack_size)                                          \
+    ({ static_assert(false, "encin_spawn_blocking_void is not implemented yet"); })
 
-#define encin_spawn1v_blocking(function, arg1, stack_size)                                         \
-    static_assert(false, "encin_spawn1v_blocking is not implemented yet");
+#define encin_spawn_detached_0(function, stack_size)                                               \
+    ({ static_assert(false, "encin_spawn_detached is not implemented yet"); })
 
-#define encin_spawn1d_blocking(function, arg1, stack_size)                                         \
-    static_assert(false, "encin_spawn1d_blocking is not implemented yet");
-
-
-#define encin_spawn2(function, arg1, arg2, stack_size)                                             \
-    static_assert(false, "encin_spawn2 is not implemented yet");
-
-#define encin_spawn2v(function, arg1, arg2, stack_size)                                            \
-    static_assert(false, "encin_spawn2v is not implemented yet");
-
-#define encin_spawn2d(function, arg1, arg2, stack_size)                                            \
-    static_assert(false, "encin_spawn2d is not implemented yet");
-
-#define encin_spawn2_blocking(function, arg1, arg2, stack_size)                                    \
-    static_assert(false, "encin_spawn2_blocking is not implemented yet");
-
-#define encin_spawn2v_blocking(function, arg1, arg2, stack_size)                                   \
-    static_assert(false, "encin_spawn2v_blocking is not implemented yet");
-
-#define encin_spawn2d_blocking(function, arg1, arg2, stack_size)                                   \
-    static_assert(false, "encin_spawn2d_blocking is not implemented yet");
+#define encin_spawn_blocking_detached_0(function, stack_size)                                      \
+    ({ static_assert(false, "encin_spawn_blocking_detached is not implemented yet"); })
 
 
-#define encin_spawn3(function, arg1, arg2, arg3, stack_size)                                       \
-    static_assert(false, "encin_spawn3 is not implemented yet");
+#define encin_spawn_1(function, arg1, stack_size)                                                  \
+    ({ static_assert(false, "encin_spawn is not implemented yet"); })
 
-#define encin_spawn3v(function, arg1, arg2, arg3, stack_size)                                      \
-    static_assert(false, "encin_spawn3v is not implemented yet");
+#define encin_spawn_blocking_1(function, arg1, stack_size)                                         \
+    ({ static_assert(false, "encin_spawn_blocking is not implemented yet"); })
 
-#define encin_spawn3d(function, arg1, arg2, arg3, stack_size)                                      \
-    static_assert(false, "encin_spawn3d is not implemented yet");
+#define encin_spawn_void_1(function, arg1, stack_size)                                             \
+    ({ static_assert(false, "encin_spawn_void is not implemented yet"); })
 
-#define encin_spawn3_blocking(function, arg1, arg2, arg3, stack_size)                              \
-    static_assert(false, "encin_spawn3_blocking is not implemented yet");
+#define encin_spawn_blocking_void_1(function, arg1, stack_size)                                    \
+    ({ static_assert(false, "encin_spawn_blocking_void is not implemented yet"); })
 
-#define encin_spawn3v_blocking(function, arg1, arg2, arg3, stack_size)                             \
-    static_assert(false, "encin_spawn3v_blocking is not implemented yet");
+#define encin_spawn_detached_1(function, arg1, stack_size)                                         \
+    ({ static_assert(false, "encin_spawn_detached is not implemented yet"); })
 
-#define encin_spawn3d_blocking(function, arg1, arg2, arg3, stack_size)                             \
-    static_assert(false, "encin_spawn3d_blocking is not implemented yet");
-
-
-#define encin_spawn4(function, arg1, arg2, arg3, arg4, stack_size)                                 \
-    static_assert(false, "encin_spawn4 is not implemented yet");
-
-#define encin_spawn4v(function, arg1, arg2, arg3, arg4, stack_size)                                \
-    static_assert(false, "encin_spawn4v is not implemented yet");
-
-#define encin_spawn4d(function, arg1, arg2, arg3, arg4, stack_size)                                \
-    static_assert(false, "encin_spawn4d is not implemented yet");
-
-#define encin_spawn4_blocking(function, arg1, arg2, arg3, arg4, stack_size)                        \
-    static_assert(false, "encin_spawn4_blocking is not implemented yet");
-
-#define encin_spawn4v_blocking(function, arg1, arg2, arg3, arg4, stack_size)                       \
-    static_assert(false, "encin_spawn4v_blocking is not implemented yet");
-
-#define encin_spawn4d_blocking(function, arg1, arg2, arg3, arg4, stack_size)                       \
-    static_assert(false, "encin_spawn4d_blocking is not implemented yet");
+#define encin_spawn_blocking_detached_1(function, arg1, stack_size)                                \
+    ({ static_assert(false, "encin_spawn_blocking_detached is not implemented yet"); })
 
 
-#define encin_spawn5(function, arg1, arg2, arg3, arg4, arg5, stack_size)                           \
-    static_assert(false, "encin_spawn5 is not implemented yet");
+#define encin_spawn_2(function, arg1, arg2, stack_size)                                            \
+    ({ static_assert(false, "encin_spawn is not implemented yet"); })
 
-#define encin_spawn5v(function, arg1, arg2, arg3, arg4, arg5, stack_size)                          \
-    static_assert(false, "encin_spawn5v is not implemented yet");
+#define encin_spawn_blocking_2(function, arg1, arg2, stack_size)                                   \
+    ({ static_assert(false, "encin_spawn_blocking is not implemented yet"); })
 
-#define encin_spawn5d(function, arg1, arg2, arg3, arg4, arg5, stack_size)                          \
-    static_assert(false, "encin_spawn5d is not implemented yet");
+#define encin_spawn_void_2(function, arg1, arg2, stack_size)                                       \
+    ({ static_assert(false, "encin_spawn_void is not implemented yet"); })
 
-#define encin_spawn5_blocking(function, arg1, arg2, arg3, arg4, arg5, stack_size)                  \
-    static_assert(false, "encin_spawn5_blocking is not implemented yet");
+#define encin_spawn_blocking_void_2(function, arg1, arg2, stack_size)                              \
+    ({ static_assert(false, "encin_spawn_blocking_void is not implemented yet"); })
 
-#define encin_spawn5v_blocking(function, arg1, arg2, arg3, arg4, arg5, stack_size)                 \
-    static_assert(false, "encin_spawn5v_blocking is not implemented yet");
+#define encin_spawn_detached_2(function, arg1, arg2, stack_size)                                   \
+    ({ static_assert(false, "encin_spawn_detached is not implemented yet"); })
 
-#define encin_spawn5d_blocking(function, arg1, arg2, arg3, arg4, arg5, stack_size)                 \
-    static_assert(false, "encin_spawn5d_blocking is not implemented yet");
+#define encin_spawn_blocking_detached_2(function, arg1, arg2, stack_size)                          \
+    ({ static_assert(false, "encin_spawn_blocking_detached is not implemented yet"); })
 
 
-#define encin_spawn6(function, arg1, arg2, arg3, arg4, arg5, arg6, stack_size)                     \
-    static_assert(false, "encin_spawn6 is not implemented yet");
+#define encin_spawn_3(function, arg1, arg2, arg3, stack_size)                                      \
+    ({ static_assert(false, "encin_spawn is not implemented yet"); })
 
-#define encin_spawn6v(function, arg1, arg2, arg3, arg4, arg5, arg6, stack_size)                    \
-    static_assert(false, "encin_spawn6v is not implemented yet");
+#define encin_spawn_blocking_3(function, arg1, arg2, arg3, stack_size)                             \
+    ({ static_assert(false, "encin_spawn_blocking is not implemented yet"); })
 
-#define encin_spawn6d(function, arg1, arg2, arg3, arg4, arg5, arg6, stack_size)                    \
-    static_assert(false, "encin_spawn6d is not implemented yet");
+#define encin_spawn_void_3(function, arg1, arg2, arg3, stack_size)                                 \
+    ({ static_assert(false, "encin_spawn_void is not implemented yet"); })
 
-#define encin_spawn6_blocking(function, arg1, arg2, arg3, arg4, arg5, arg6, stack_size)            \
-    static_assert(false, "encin_spawn6_blocking is not implemented yet");
+#define encin_spawn_blocking_void_3(function, arg1, arg2, arg3, stack_size)                        \
+    ({ static_assert(false, "encin_spawn_blocking_void is not implemented yet"); })
 
-#define encin_spawn6v_blocking(function, arg1, arg2, arg3, arg4, arg5, arg6, stack_size)           \
-    static_assert(false, "encin_spawn6v_blocking is not implemented yet");
+#define encin_spawn_detached_3(function, arg1, arg2, arg3, stack_size)                             \
+    ({ static_assert(false, "encin_spawn_detached is not implemented yet"); })
 
-#define encin_spawn6d_blocking(function, arg1, arg2, arg3, arg4, arg5, arg6, stack_size)           \
-    static_assert(false, "encin_spawn6d_blocking is not implemented yet");
+#define encin_spawn_blocking_detached_3(function, arg1, arg2, arg3, stack_size)                    \
+    ({ static_assert(false, "encin_spawn_blocking_detached is not implemented yet"); })
+
+
+#define encin_spawn_4(function, arg1, arg2, arg3, arg4, stack_size)                                \
+    ({ static_assert(false, "encin_spawn is not implemented yet"); })
+
+#define encin_spawn_blocking_4(function, arg1, arg2, arg3, arg4, stack_size)                       \
+    ({ static_assert(false, "encin_spawn_blocking is not implemented yet"); })
+
+#define encin_spawn_void_4(function, arg1, arg2, arg3, arg4, stack_size)                           \
+    ({ static_assert(false, "encin_spawn_void is not implemented yet"); })
+
+#define encin_spawn_blocking_void_4(function, arg1, arg2, arg3, arg4, stack_size)                  \
+    ({ static_assert(false, "encin_spawn_blocking_void is not implemented yet"); })
+
+#define encin_spawn_detached_4(function, arg1, arg2, arg3, arg4, stack_size)                       \
+    ({ static_assert(false, "encin_spawn_detached is not implemented yet"); })
+
+#define encin_spawn_blocking_detached_4(function, arg1, arg2, arg3, arg4, stack_size)              \
+    ({ static_assert(false, "encin_spawn_blocking_detached is not implemented yet"); })
+
+
+#define encin_spawn_5(function, arg1, arg2, arg3, arg4, arg5, stack_size)                          \
+    ({ static_assert(false, "encin_spawn is not implemented yet"); })
+
+#define encin_spawn_blocking_5(function, arg1, arg2, arg3, arg4, arg5, stack_size)                 \
+    ({ static_assert(false, "encin_spawn_blocking is not implemented yet"); })
+
+#define encin_spawn_void_5(function, arg1, arg2, arg3, arg4, arg5, stack_size)                     \
+    ({ static_assert(false, "encin_spawn_void is not implemented yet"); })
+
+#define encin_spawn_blocking_void_5(function, arg1, arg2, arg3, arg4, arg5, stack_size)            \
+    ({ static_assert(false, "encin_spawn_blocking_void is not implemented yet"); })
+
+#define encin_spawn_detached_5(function, arg1, arg2, arg3, arg4, arg5, stack_size)                 \
+    ({ static_assert(false, "encin_spawn_detached is not implemented yet"); })
+
+#define encin_spawn_blocking_detached_5(function, arg1, arg2, arg3, arg4, arg5, stack_size)        \
+    ({ static_assert(false, "encin_spawn_blocking_detached is not implemented yet"); })
+
+
+#define encin_spawn_6(function, arg1, arg2, arg3, arg4, arg5, arg6, stack_size)                    \
+    ({ static_assert(false, "encin_spawn is not implemented yet"); })
+
+#define encin_spawn_blocking_6(function, arg1, arg2, arg3, arg4, arg5, arg6, stack_size)           \
+    ({ static_assert(false, "encin_spawn_blocking is not implemented yet"); })
+
+#define encin_spawn_void_6(function, arg1, arg2, arg3, arg4, arg5, arg6, stack_size)               \
+    ({ static_assert(false, "encin_spawn_void is not implemented yet"); })
+
+#define encin_spawn_blocking_void_6(function, arg1, arg2, arg3, arg4, arg5, arg6, stack_size)      \
+    ({ static_assert(false, "encin_spawn_blocking_void is not implemented yet"); })
+
+#define encin_spawn_detached_6(function, arg1, arg2, arg3, arg4, arg5, arg6, stack_size)           \
+    ({ static_assert(false, "encin_spawn_detached is not implemented yet"); })
+
+#define encin_spawn_blocking_detached_6(function, arg1, arg2, arg3, arg4, arg5, arg6, stack_size)  \
+    ({ static_assert(false, "encin_spawn_blocking_detached is not implemented yet"); })
