8868fb71540afd9d4f1acffd15c8df8f31449ae7
Umut <umutsahin@protonmail.com>
2021-03-28 21:22:25 +0300

    fix an unlikely data race in handling-signals example

--- examples/handling-signals.c
+++ examples/handling-signals.c
@@ -14,12 +14,11 @@ int encin(void) {
     size_t press_count = 0;
     encin_signal(SIGINT, sigint_handler, &press_count);
 
-    while (atomic_load_explicit(&press_count, memory_order_relaxed) < 42) {
-        printf(
-            "You pressed CTRL+C %zu times...\n",
-            atomic_load_explicit(&press_count, memory_order_relaxed)
-        );
+    size_t local_press_count = atomic_load_explicit(&press_count, memory_order_relaxed);
+    while (local_press_count < 42) {
+        printf("You pressed CTRL+C %zu times...\n", local_press_count);
         usleep(100000);
+        local_press_count = atomic_load_explicit(&press_count, memory_order_relaxed);
     }
 
     return 0;
