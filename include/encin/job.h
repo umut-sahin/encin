c4c91658e736b5401b753be7fc657e9b5bd032b1
Umut <umutsahin@protonmail.com>
2021-04-19 12:47:39 +0300

    add minimum required fields of encin_job

--- include/encin/job.h
+++ include/encin/job.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#include <encin/result.h>
+#include <stdbool.h>
+#include <ucontext.h>
+
 typedef struct encin_job {
+
+    encin_result encin_status_;
+    int errno_;
+
+    bool is_scheduled;
+    ucontext_t context;
 
 } encin_job;
